cedf9bee4794109d95368cec96dbe79009041877
Simon Eskildsen <sirup@sirupsen.com>
2013-12-08 20:24:45 +0000

    store/redis: allow custom key prefix

--- lib/progressrus/store/redis.rb
+++ lib/progressrus/store/redis.rb
@@ -2,38 +2,37 @@ module Progressrus
   module Store
     class Redis < Base
       attr_accessor :expire
-      attr_reader :redis
+      attr_reader :redis, :prefix
 
-      KEY_PREFIX = "progressrus"
-
-      def initialize(redis = ::Redis.new, expire = 60 * 30)
+      def initialize(redis = ::Redis.new, expire = 60 * 30, prefix = "progressrus")
         @redis = redis
         @expire = expire
+        @prefix = prefix
       end
 
       def persist(progress)
-        @redis.hset(key(progress.scope), progress.id, {
+        redis.hset(key(progress.scope), progress.id, {
           count:      progress.count,
           total:      progress.total,
           started_at: progress.started_at
         }.to_json)
-        @redis.expire(key(progress.scope), @expire)
+        redis.expire(key(progress.scope), expire)
       end
 
       def scope(scope)
-        scope = @redis.hgetall(key(scope))
+        scope = redis.hgetall(key(scope))
         scope.each_pair { |id, value| 
           scope[id] = Tick.new(JSON.parse(value, symbolize_names: true))
         }
       end
 
       def flush
-        @redis.flushall
+        redis.flushall
       end
 
       private
       def key(scope)
-        "#{KEY_PREFIX}:#{scope.join(":")}"
+        "#{prefix}:#{scope.join(":")}"
       end
     end
   end
